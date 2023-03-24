5b223c9a632a81a5ae5c619fc9ea6f1b246201eb
Jan Malčák <services@malcak.cz>
2023-03-24 01:24:48 +0100

    Allow API routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,17 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 	root to: 'pages#home'
 
+	namespace :api do
+		# I don't want users to be created/deleted through the API
+		resources :users, only: [:index, :update]
+		resources :auth, only: [:create, :destroy]
+		resources :people, only: [:index, :show, :create, :update, :destroy] do
+			resources :addresses, only: [:index, :show, :create, :update, :destroy]
+			resources :emails, only: [:index, :show, :create, :update, :destroy]
+			resources :phone_numbers, only: [:index, :show, :create, :update, :destroy]
+		end
+	end
+
 	resources :people do
 		resources :addresses, only: [:new, :edit, :create, :update, :destroy]
 		resources :emails, only: [:new, :edit, :create, :update, :destroy]
